46e309ed38e72e17bd62bd8a9ec07fb5df6ddd62
Ismail <ismailisims1@gmail.com>
2025-12-23 18:55:00 +0530

    Initial commit

---
+++ js/intro_data.js
@@ -0,0 +1,29 @@
+// js/intro_data.js
+
+export const INTRO_CONTENT = {
+    // Top Blue Bar Text
+    headerTitle: "External Taper",
+    
+    // Button Text & Link
+    buttonLabel: "Experiment",
+    buttonLink: "lab.html",
+
+    // Main Page Content
+    mainHeading: "External Taper Measurement Using vernier Caliper",
+    
+    // Aim Section
+    aimTitle: "Aim",
+    aimContent: "To measure the external taper of a given object using vernier caliper.",
+
+    // Acknowledgement Section
+    ackTitle: "Acknowledgement",
+    ackContent: "The Department of Production Technology appreciates the following students for their support in the development of Metrology and measurements virtual laboratory experiment.",
+    
+    // Student List
+    students: [
+        "Mothinath G - 2023507009",
+        "Madusudarsanan J-2023507021",
+        "Prabakaran K -2023507302",
+        "Venugopal R-2023507307"
+    ]
+};
